d7283f5d22fc9546bf2171be9ded375f33d53e53
M. Kamil <mk.kamiil@gmail.com>
2021-04-21 14:00:08 +0700

    initialize project

---
+++ src/main/java/com/enigma/pocket/entity/Customer.java
@@ -0,0 +1,101 @@
+package com.enigma.pocket.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "m_customer")
+public class Customer {
+
+    @Id //menandai kalau primary key
+    @Column(name = "customer_id")
+    private Integer customerId;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    private String address;
+    private Integer status;
+    private String username;
+    private String password;
+    private String email;
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/test/java/com/enigma/pocket/GoldPocketBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.pocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldPocketBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
